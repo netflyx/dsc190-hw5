65fe1d511f0acc4803439768aaba8707ce31bb28
Alyx Wikarsa <alyx.wikarsa@gmail.com>
2026-05-05 12:54:38 -0700

    fixing things
    s=.

--- .github/workflows/main.py
+++ .github/workflows/main.py
@@ -1,8 +1,6 @@
-import os
-import sys
-
 def main():
-    unused_variable = 123
-    print(  "Hello from dsc190-hw5!"  )
+    print("Hello from dsc190-hw5!")
 
-main()
+
+if __name__ == "__main__":
+    main()
